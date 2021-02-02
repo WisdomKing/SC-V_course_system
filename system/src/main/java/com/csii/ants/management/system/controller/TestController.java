8543b6a409174460b7409a24498d8a56d5262b69
wisdom <wisdomking176@163.com>
2021-02-02 10:51:17 +0800

    1.在gateway增加system路由转发配置，调试成功

--- system/src/main/java/com/csii/ants/management/system/controller/TestController.java
+++ system/src/main/java/com/csii/ants/management/system/controller/TestController.java
@@ -6,11 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author 曾晓明
  * @date 2021/2/2
- * @Description:
+ * @Description:测试
  */
 
 /**
  * 如果返回的是json数据用 @RestController,如果返回的是页面用 @Controller;
+ */
+
+/**
+ * 路由跳转的三种方法
+ * 第一种方法，在方法前面加 @RequestMapping("/system/test")，但是过于繁琐
+ * 第二种方法，在类前面加@RequestMapping("/system")，类多了也繁琐
+ * 第三种方法，在application.properties里加server.servlet.context-path=/system
  */
 @RestController
 public class TestController {
