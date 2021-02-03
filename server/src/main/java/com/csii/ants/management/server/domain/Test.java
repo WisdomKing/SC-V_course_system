7cc0c0e9ff9688d7b08dc8ddebceb8c54c5b9c4a
wisdom <wisdomking176@163.com>
2021-02-03 10:41:27 +0800

    1.新建server子模块[将表的增删改查集成]

---
+++ server/src/main/java/com/csii/ants/management/server/domain/Test.java
@@ -0,0 +1,27 @@
+package com.csii.ants.management.server.domain;
+
+/**
+ * @author 曾晓明
+ * @date 2021/2/3
+ * @Description:
+ */
+public class Test {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ server/src/main/java/com/csii/ants/management/server/service/TestService.java
@@ -0,0 +1,23 @@
+package com.csii.ants.management.server.service;
+
+import com.csii.ants.management.server.domain.Test;
+import com.csii.ants.management.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author 曾晓明
+ * @date 2021/2/3
+ * @Description:
+ */
+@Service
+public class TestService {
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
