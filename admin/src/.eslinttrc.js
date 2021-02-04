d42b2eae569f83bb8d1ffd71e4347c5008459350
wisdom <wisdomking176@163.com>
2021-02-04 11:24:14 +0800

    1.安装vue-router npm install --save vue-router

--- admin/src/.eslinttrc.js
+++ admin/src/.eslinttrc.js
@@ -8,14 +8,15 @@ module.exports = {
         'eslint:recommended'
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-console': 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-undef': 'off',
         'vue/no-unused-vars': 'off',
         'vue/require-v-for-key': 'off',
+        'no-unused-vars': 'off',
+        'vue/no-unused-components': 'off'
     },
     parserOptions: {
         parser: 'babel-eslint'
     }
 };
-
