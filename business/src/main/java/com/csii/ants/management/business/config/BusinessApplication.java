3cb98238648758aeb1aaebeaf73e10b77db6ba87
wisdom <wisdomking176@163.com>
2021-02-18 09:41:11 +0800

    1.增加business模块和初始启动代码

---
+++ business/src/main/java/com/csii/ants/management/business/config/BusinessApplication.java
@@ -0,0 +1,29 @@
+package com.csii.ants.management.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author ZxM
+ * @date 2021/2/1
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.csii.ants.management")
+@MapperScan("com.csii.ants.management.server.mapper")
+public class BusinessApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("Business 地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+}
--- business/src/main/java/com/csii/ants/management/business/controller/TestController.java
+++ business/src/main/java/com/csii/ants/management/business/controller/TestController.java
@@ -1,10 +1,8 @@
-package com.csii.ants.management.system.controller;
+package com.csii.ants.management.business.controller;
 
 import com.csii.ants.management.server.domain.Test;
 import com.csii.ants.management.server.service.TestService;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -36,13 +34,4 @@ public class TestController {
     public List<Test> test() {
         return testService.list();
     }
-/*
-    //查询用户(从eclipse物流项目copy过来的模块)
-    @ResponseBody
-    @RequestMapping("query")
-    public String query(WarehouseInfo record) {
-        DataGridResult rs = warehouseService.query(record);
-        return JSON.toJSONString(rs);
-    }
- */
 }
