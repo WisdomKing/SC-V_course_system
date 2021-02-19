3004aa81843133d8bcf0e325574c29d30d4b1658
wisdom <wisdomking176@163.com>
2021-02-19 15:09:31 +0800

    1.增加了dto层，用于controller和service 2.增加AnnouncementDto 3.修改了AnnouncemenService，将返回Announcement改成返回AnnouncementDto

--- business/src/main/java/com/csii/ants/management/business/controller/admin/AnnouncementController.java
+++ business/src/main/java/com/csii/ants/management/business/controller/admin/AnnouncementController.java
@@ -1,6 +1,7 @@
-package com.csii.ants.management.business.controller;
+package com.csii.ants.management.business.controller.admin;
 
 import com.csii.ants.management.server.domain.Announcement;
+import com.csii.ants.management.server.dto.AnnouncementDto;
 import com.csii.ants.management.server.service.AnnouncementService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,27 +12,18 @@ import java.util.List;
 /**
  * @author ZxM
  * @date 2021/2/19
- * @Description:测试
+ * @Description:公告模块
  */
 
-/**
- * 如果返回的是json数据用 @RestController,如果返回的是页面用 @Controller;
- */
-
-/**
- * 路由跳转的三种方法
- * 第一种方法，在方法前面加 @RequestMapping("/system/test")，但是过于繁琐
- * 第二种方法，在类前面加@RequestMapping("/system")，类多了也繁琐
- * 第三种方法，在application.properties里加server.servlet.context-path=/system
- */
 @RestController
+@RequestMapping("/admin")
 public class AnnouncementController {
 
     @Resource
     private AnnouncementService announcementService;
 
     @RequestMapping("/announcement")
-    public List<Announcement> test() {
+    public List<AnnouncementDto> test() {
         return announcementService.list();
     }
 }
--- server/src/main/java/com/csii/ants/management/server/service/AnnouncementService.java
+++ server/src/main/java/com/csii/ants/management/server/service/AnnouncementService.java
@@ -2,10 +2,13 @@ package com.csii.ants.management.server.service;
 
 import com.csii.ants.management.server.domain.Announcement;
 import com.csii.ants.management.server.domain.AnnouncementExample;
+import com.csii.ants.management.server.dto.AnnouncementDto;
 import com.csii.ants.management.server.mapper.AnnouncementMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,9 +21,17 @@ public class AnnouncementService {
     @Resource
     private AnnouncementMapper announcementMapper;
 
-    public List<Announcement> list(){
+    public List<AnnouncementDto> list(){
         AnnouncementExample announcementExample = new AnnouncementExample();
-        announcementExample.createCriteria().andAnnouncementtypeEqualTo("公共公告");
-        return announcementMapper.selectByExample(announcementExample);
+        List<Announcement> announcementList = announcementMapper.selectByExample(announcementExample);
+        List<AnnouncementDto> announcementDtoList = new ArrayList<AnnouncementDto>();
+        for (int i = 0; i < announcementList.size(); i++) {
+            Announcement announcement = announcementList.get(i);
+            AnnouncementDto announcementDto = new AnnouncementDto();
+            //BeanUtils.copyProperties(来源,目标);
+            BeanUtils.copyProperties(announcement,announcementDto);
+            announcementDtoList.add(announcementDto);
+        }
+        return announcementDtoList;
     }
 }
