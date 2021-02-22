77805fca63cf9ce776bd71bdd8b2af87303e1ce2
wisdom <wisdomking176@163.com>
2021-02-22 15:54:48 +0800

    1.解决gateway跨域问题

--- server/src/main/java/com/csii/ants/management/server/config/CorsConfig.java
+++ server/src/main/java/com/csii/ants/management/server/config/CorsConfig.java
@@ -1,26 +1,26 @@
-package com.csii.ants.management.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-/**
- * @author ZxM
- * @date 2021/2/20
- * @Description:解决跨域问题
- */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-//                .allowedOrigins("*")
-                .allowedOriginPatterns("*")
-                .allowedHeaders(CorsConfiguration.ALL)
-                .allowedMethods(CorsConfiguration.ALL)
-                .allowCredentials(true)
-                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
-    }
-}
+//package com.csii.ants.management.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+///**
+// * @author ZxM
+// * @date 2021/2/20
+// * @Description:解决跨域问题
+// */
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+////                .allowedOrigins("*")
+//                .allowedOriginPatterns("*")
+//                .allowedHeaders(CorsConfiguration.ALL)
+//                .allowedMethods(CorsConfiguration.ALL)
+//                .allowCredentials(true)
+//                .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
+//    }
+//}
