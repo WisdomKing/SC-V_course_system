77213e0696cd17e09853092ff318c712b83a1313
wisdom <wisdomking176@163.com>
2021-02-25 16:48:12 +0800

    1.生成器开发：增加Dto生成

--- generator/src/main/java/com/csii/ants/management/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/csii/ants/management/generator/server/ServerGenerator.java
@@ -1,11 +1,12 @@
 package com.csii.ants.management.generator.server;
 
+import com.csii.ants.management.generator.util.DbUtil;
+import com.csii.ants.management.generator.util.Field;
 import com.csii.ants.management.generator.util.FreeMarkerUtil;
 import freemarker.template.TemplateException;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author ZxM
@@ -18,20 +19,29 @@ public class ServerGenerator {
      * 支持重复生成
      */
     static String MODULE = "business";
+    static String toDtoPath = "server\\src\\main\\java\\com\\csii\\ants\\management\\server\\dto\\";
     static String toServicePath = "server\\src\\main\\java\\com\\csii\\ants\\management\\server\\service\\";
     static String toControllerPath =MODULE+ "\\src\\main\\java\\com\\csii\\ants\\management\\"+MODULE+"\\controller\\admin\\";
 
-    public static void main(String[] args) throws IOException, TemplateException {
+    public static void main(String[] args) throws Exception {
         String Domain="Headline";
         String domain="headline";
         String tableNameCn="头条";
         String module=MODULE;
+        List<Field> fieldList=DbUtil.getColumnByTableName(domain);
+        Set<String> typeSet=getJavaTypes(fieldList);
 
         Map<String,Object> map=new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
         map.put("tableNameCn",tableNameCn);
         map.put("module",module);
+        map.put("fieldList",fieldList);
+        map.put("typeSet",typeSet);
+
+        //生成Dto
+        FreeMarkerUtil.initConfig("dto.ftl");
+        FreeMarkerUtil.generator(toDtoPath+Domain+"Dto.java",map);
 
         //生成service
         FreeMarkerUtil.initConfig("service.ftl");
@@ -40,5 +50,17 @@ public class ServerGenerator {
         //生成controller
         FreeMarkerUtil.initConfig("controller.ftl");
         FreeMarkerUtil.generator(toControllerPath+Domain+"Controller.java",map);
+    }
+
+    /**
+     * 获取所有的Java类型，使用Set去重
+     */
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
--- server/src/main/java/com/csii/ants/management/server/dto/HeadlineDto.java
+++ server/src/main/java/com/csii/ants/management/server/dto/HeadlineDto.java
@@ -1,59 +1,77 @@
 package com.csii.ants.management.server.dto;
 
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class HeadlineDto {
+
+    /**
+    * ID
+    */
     private String id;
 
+    /**
+    * 上传时间
+    */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createdtime;
 
+    /**
+    * 修改时间
+    */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updatedtime;
 
+    /**
+    * 详情
+    */
     private String details;
 
     public String getId() {
-        return id;
+    return id;
     }
 
     public void setId(String id) {
-        this.id = id;
+    this.id = id;
     }
 
     public Date getCreatedtime() {
-        return createdtime;
+    return createdtime;
     }
 
     public void setCreatedtime(Date createdtime) {
-        this.createdtime = createdtime;
+    this.createdtime = createdtime;
     }
 
     public Date getUpdatedtime() {
-        return updatedtime;
+    return updatedtime;
     }
 
     public void setUpdatedtime(Date updatedtime) {
-        this.updatedtime = updatedtime;
+    this.updatedtime = updatedtime;
     }
 
     public String getDetails() {
-        return details;
+    return details;
     }
 
     public void setDetails(String details) {
-        this.details = details;
+    this.details = details;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", createdtime=").append(createdtime);
-        sb.append(", updatedtime=").append(updatedtime);
-        sb.append(", details=").append(details);
-        sb.append("]");
-        return sb.toString();
-    }
+
+@Override
+public String toString() {
+StringBuilder sb = new StringBuilder();
+sb.append(getClass().getSimpleName());
+sb.append(" [");
+sb.append("Hash = ").append(hashCode());
+    sb.append(", id=").append(id);
+    sb.append(", createdtime=").append(createdtime);
+    sb.append(", updatedtime=").append(updatedtime);
+    sb.append(", details=").append(details);
+sb.append("]");
+return sb.toString();
+}
+
 }
