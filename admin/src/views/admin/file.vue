1073802b15e2d76c7d5a7427dfc2b23d9b0b2bcf
wisdom <wisdomking176@163.com>
2021-05-07 12:31:51 +0800

    1.文件上传功能开发：去掉文件管理的新增、修改、删除功能

--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -1,20 +1,14 @@
 <template>
   <div>
-    <!-- 刷新按钮 -->
     <p>
-    <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-      <i class="ace-icon fa fa-edit"></i>
-      新增
-    </button>
-      &nbsp;
-    <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
-      <i class="ace-icon fa fa-refresh"></i>
-      刷新
-    </button>
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
     </p>
-    <!-- 分页插件 -->
-    <pagination ref="pagination" v-bind:list="list" v-bind:item-count="5"></pagination>
-    <!-- 表单数据 -->
+
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
@@ -23,7 +17,6 @@
         <th>文件名</th>
         <th>后缀</th>
         <th>大小</th>
-        <th>操作按钮</th>
       </tr>
       </thead>
 
@@ -34,170 +27,49 @@
         <td>{{file.name}}</td>
         <td>{{file.suffix}}</td>
         <td>{{file.size}}</td>
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(file)" class="btn btn-xs btn-info">
-              <!--详情-->
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-            <button v-on:click="del(file.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-        </td>
       </tr>
       </tbody>
     </table>
-
-    <!-- 模态框 -->
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">文件</h4>
-          </div>
-          <div class="modal-body">
-            <!-- 表单 -->
-            <form class="form-horizontal">
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">相对路径</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.path" class="form-control" placeholder="相对路径">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">文件名</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.name" class="form-control" placeholder="文件名">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">后缀</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.suffix" class="form-control" placeholder="后缀">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">大小</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.size" class="form-control" placeholder="大小">
-                  </div>
-                </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
   import Pagination from "../../components/pagination";
   export default {
-    name: 'file-file',
     components: {Pagination},
-    data:function(){
-      return{
-        file:{},
-        files:[]
+    name: "file-file",
+    data: function() {
+      return {
+        file: {},
+        files: [],
       }
     },
-    mounted: function () {
-      let _this=this;
-      //自定义初始每页5条
-      _this.$refs.pagination.size=5;
-      _this.list();
-      },
+    mounted: function() {
+      let _this = this;
+      _this.$refs.pagination.size = 5;
+      _this.list(1);
+      // sidebar激活样式方法一
+      // this.$parent.activeSidebar("file-file-sidebar");
+
+    },
     methods: {
-      /**
-       * 点击新增
-       */
-      add(){
-        let _this=this;
-        //模态框打开时清空上次的数据
-        _this.file={}
-        $("#form-modal").modal("show");
-      },
-      /**
-       * 点击编辑
-       */
-      edit(file){
-        let _this=this;
-        //将数据带到模态框里
-        _this.file=$.extend({},file);
-        $("#form-modal").modal("show");
-      },
       /**
        * 列表查询
        */
-      list(page){
-        let _this=this;
+      list(page) {
+        let _this = this;
         Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER+'/file/admin/file/list',{
-          page:page,
-          size:_this.$refs.pagination.size,
-        }).then((respond)=>{
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/list', {
+          page: page,
+          size: _this.$refs.pagination.size,
+        }).then((response)=>{
           Loading.hide();
-          let resp=respond.data;
-          _this.files=resp.content.list;
-          //重新渲染？5.5-1155
-          _this.$refs.pagination.render(page,resp.content.total);
-        })
-      },
-      /**
-       * 点击保存
-       */
-      save(page){
-        let _this=this;
-        // 保存校验，非空和长度
-        if (1 != 1
-          || !Validator.require(_this.file.path, "相对路径")
-          || !Validator.length(_this.file.path, "相对路径", 1, 100)
-          || !Validator.length(_this.file.name, "文件名", 1, 100)
-          || !Validator.length(_this.file.suffix, "后缀", 1, 10)
-        ) {
-          return;
-        }
+          let resp = response.data;
+          _this.files = resp.content.list;
+          _this.$refs.pagination.render(page, resp.content.total);
 
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER+'/file/admin/file/save',
-_this.file).then((respond)=>{
-          Loading.hide();
-          // console.log("保存文件列表结果:",respond);
-          let resp=respond.data;
-          if (resp.success){
-            //如果成功了，隐藏modal和刷新列表
-            $("#form-modal").modal("hide");
-            _this.list(1);
-            Toast.success("保存成功");
-          }else {
-            Toast.warning(resp.message)
-          }
         })
       },
-      /**
-       * 点击删除
-       */
-      del(id){
-        let _this=this;
-        Confirm.show("删除文件后不可恢复，确认删除?",function () {
-          Loading.show();
-          _this.$ajax.delete(process.env.VUE_APP_SERVER+'/file/admin/file/delete/'+id).then((respond)=>{
-            Loading.hide();
-            // console.log("删除文件列表结果:",respond);
-            let resp=respond.data;
-            if (resp.success){
-              _this.list(1);
-              Toast.success("删除成功");
-            }
-          })
-        });
-      }
     }
   }
 </script>
--- file/src/main/java/com/csii/ants/management/file/controller/admin/FileController.java
+++ file/src/main/java/com/csii/ants/management/file/controller/admin/FileController.java
@@ -40,37 +40,4 @@ public class FileController {
         return responseDto;
     }
 
-    /**
-     * 保存，id有值时更新，无值时新增
-     * @param fileDto
-     * @return responseDto
-     */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto) {
-        Log.info("fileDto:{}",fileDto);
-
-        // 保存校验
-        ValidatorUtil.require(fileDto.getPath(), "相对路径");
-        ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-
-        ResponseDto responseDto=new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    /**
-     * 删除
-     * @param id
-     * @return responseDto
-     */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        Log.info("id:{}",id);
-        ResponseDto responseDto=new ResponseDto();
-        fileService.delete(id);
-        return responseDto;
-    }
 }
